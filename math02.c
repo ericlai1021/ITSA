9cf7ab86cb95f8cf60ba4f0fc747e534a8ae141f
ericlai1021 <53555490+ericlai1021@users.noreply.github.com>
2020-01-17 11:39:42 +0800

    加分題

---
+++ math02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main()
+{
+	float a, b;
+	scanf("%f %f",&a, &b);
+	printf("%.1f\n",(a*b)/2);
+}
---
+++ math06.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%.1f\n",(double)a*1.6);
+}
